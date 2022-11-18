5bc2e8b625660c92b8cd53ba1f6d489844cb4ea1
mapfiltermagic <ajmurill@ucsd.edu>
2022-11-17 20:12:05 -0800

    Created start-intermediary

---
+++ start-intermediary/src/main/java/com/mapfiltermagic/openainitializr/OpenAinitializrApplication.java
@@ -0,0 +1,13 @@
+package com.mapfiltermagic.openainitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAinitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAinitializrApplication.class, args);
+	}
+
+}

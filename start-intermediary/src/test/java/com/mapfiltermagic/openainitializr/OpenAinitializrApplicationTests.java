1935883ed5f3a5c4b7f219b0e4463127a94a7517
mapfiltermagic <ajmurill@ucsd.edu>
2022-11-17 20:12:05 -0800

    Created start-intermediary

---
+++ start-intermediary/src/test/java/com/mapfiltermagic/openainitializr/OpenAinitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapfiltermagic.openainitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAinitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

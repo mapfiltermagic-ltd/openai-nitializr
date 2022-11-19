18c1d55943b56bd87c99a36f0f030339510dad34
mapfiltermagic <ajmurill@ucsd.edu>
2022-11-19 11:34:16 -0800

    Added basic webflux security config

---
+++ start-intermediary/src/main/java/com/mapfiltermagic/startintermediary/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.mapfiltermagic.startintermediary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain webHttpSecurity(ServerHttpSecurity http) {                       
+        http.cors();
+                                                 
+        return http.build();
+    }
+
+}

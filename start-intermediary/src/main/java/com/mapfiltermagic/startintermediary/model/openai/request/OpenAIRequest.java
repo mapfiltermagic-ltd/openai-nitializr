4cb261d33561edd26089b0de784aef38a6389cc9
mapfiltermagic <ajmurill@ucsd.edu>
2022-11-22 22:48:45 -0800

    Implemented OpenAI Service

--- start-intermediary/src/main/java/com/mapfiltermagic/startintermediary/model/openai/request/OpenAIRequest.java
+++ start-intermediary/src/main/java/com/mapfiltermagic/startintermediary/model/openai/request/OpenAIRequest.java
@@ -1,24 +1,32 @@
 package com.mapfiltermagic.startintermediary.model.openai.request;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonAlias;
 
-@Setter
-@Getter
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class OpenAIRequest {
 
-    private int maxTokens;
-    private int temperature;
-    private int topP;
-    private int n;
-    private int logprobs;
+    @JsonAlias("max_tokens")
+    private int maxTokens; // maximum number of tokens to generate in the completion
 
-    private boolean stream;
+    @JsonAlias("top_p")
+    private int topP; // alternative to sampling with temperature, called nucleus sampling
 
-    private String model;
-    private String prompt;
-    private String stop;
+    private int temperature; // what sampling temperature to use. Higher values means the model will take more risks
+    private int n; // how many completions to generate for each prompt
+    private int logprobs; // include the log probabilities on the logprobs most likely tokens, as well the chosen tokens
+
+    private boolean stream; // whether to stream back partial progress
+
+    private String model; // id of the model to use
+    private String prompt; // the prompt(s) to generate completions for, encoded as a string, array of strings, etc.
+    private String stop; // up to 4 sequences where the API will stop generating further tokens
 
 }
---
+++ start-intermediary/src/main/java/com/mapfiltermagic/startintermediary/service/rest/openai/OpenAIService.java
@@ -0,0 +1,89 @@
+package com.mapfiltermagic.startintermediary.service.rest.openai;
+
+import java.util.function.Consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.mapfiltermagic.startintermediary.model.openai.request.OpenAIRequest;
+import com.mapfiltermagic.startintermediary.model.openai.response.OpenAIResponse;
+import com.mapfiltermagic.startintermediary.util.WebClientUtil;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Handles all outward REST calls to the OpenAI API.
+ */
+@Slf4j
+@Service
+public class OpenAIService {
+
+    private static final String CREATE_COMPLETION_URI = "/v1/completions";
+    private static final String BEARER_TOKEN_PREFIX = "Bearer ";
+
+    private final String secretKey;
+
+    private final WebClient webClient;
+
+    /**
+     *
+     * @param baseUrl
+     * @param secretKey
+     * @param webClient
+     */
+    @Autowired
+    public OpenAIService(@Value("${openai.base-url}") String baseUrl, @Value("${openai.secret-key") String secretKey) {
+        this.secretKey = secretKey;
+        this.webClient = WebClientUtil.getWebClient(baseUrl);
+    }
+
+    /**
+     * Makes a POST to the completion endpoint.
+     * 
+     * @param openAIRequest the {@link OpenAIRequest} that includes set parameters to influence our result
+     * @return the populated {@link OpenAIResponse}
+     */
+    public OpenAIResponse createCompletion(OpenAIRequest openAIRequest) {
+        try {
+            OpenAIResponse openAIResponse = webClient.post()
+                .uri(CREATE_COMPLETION_URI)
+                .headers(getCreateCompletionHeaders())
+                .retrieve()
+                .bodyToMono(OpenAIResponse.class)
+                .block();
+
+            log.debug("Successfully called createCompletion endpoint");
+
+            return openAIResponse;
+        } catch (WebClientResponseException ex) {
+            log.error("Encountered unexpected exception when calling createCompletion endpoint");
+
+            throw new ResponseStatusException(HttpStatus.valueOf(ex.getRawStatusCode()), ex.getMessage());
+        }
+    }
+
+    /**
+     * Builds out the headers needed in {@link #createCompletion()}
+     * 
+     * @return the required headers consumer
+     */
+    private Consumer<HttpHeaders> getCreateCompletionHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add(HttpHeaders.AUTHORIZATION, BEARER_TOKEN_PREFIX + secretKey);
+
+        Consumer<HttpHeaders> headerConsumer = header -> {
+            headers.forEach((key, value) -> header.add(key, headers.getFirst(key)));
+        };
+
+        return headerConsumer;
+    }
+
+}
